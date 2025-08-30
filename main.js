c4a8547f5528e0f90380f83c952bfee75a639898
Pedro Amaral <pedrovitorfa.5@gmail.com>
2025-08-29 23:08:52 -0300

    começando main.js

---
+++ main.js
@@ -0,0 +1,30 @@
+import { generateReturnsArray } from "./src/investimentGoals";
+
+const calculateButton = document.getElementById("caculate-results");
+
+function renderProgression() {
+  const startingAmmount = Number(
+    document.getElementById("starting-amount").value
+  );
+  const additionalContribution = Number(
+    document.getElementById("additional-contribution").value
+  );
+  const timeAmount = Number(document.getElementById("time-amount").value);
+  const timeAmountPeriod = document.getElementById("time-amount-period").value;
+  const returnRate = Number(document.getElementById("return-rate").value);
+  const returnRatePeriod = document.getElementById("evaluation-period").value;
+  const taxRate = Number(document.getElementById("tax-rate").value);
+
+  const returnsArray = generateReturnsArray(
+    startingAmmount,
+    timeAmount,
+    timeAmountPeriod,
+    additionalContribution,
+    returnRate,
+    returnRatePeriod
+  );
+
+  console.log(returnsArray);
+}
+
+calculateButton.addEventListener('click', renderProgression)
